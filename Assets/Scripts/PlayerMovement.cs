c42ab904ee86d719b117ad33bc83a12565333b2f
Stefan <stefanhulspas@gmail.com>
2021-11-09 12:39:09 +0100

    13 Apply audio on input

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,28 +3,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody), typeof(AudioSource))]
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody _rigidbody;
+    private AudioSource _audioSource;
 
     [SerializeField] private float _upwardThrustPower = 1000;
     [SerializeField] private float _rotationThrustPower = 50;
-    private Vector3 _rotationVelocity;
+
+    private bool _thrusterIsActive;
 
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        _thrusterIsActive = false;
         ProcessInput();
+        HandleAudio();
     }
 
-    private void ProcessInput() {
+	private void HandleAudio()
+	{
+        if (_thrusterIsActive && !_audioSource.isPlaying) {
+            _audioSource.Play();
+		} 
+        else if (!_thrusterIsActive && _audioSource.isPlaying) {
+            _audioSource.Stop();
+		}
+	}
+
+	private void ProcessInput() {
         ProcessUpwardsForce();
         ProcessRotationForce();
 	}
@@ -34,6 +49,7 @@ public class PlayerMovement : MonoBehaviour
         Vector3 upwardsForce = Vector3.up * _upwardThrustPower * Time.deltaTime;
         if (Input.GetKey(KeyCode.UpArrow))
         {
+            _thrusterIsActive = true;
             _rigidbody.AddRelativeForce(upwardsForce);
         }
     }
@@ -43,10 +59,12 @@ public class PlayerMovement : MonoBehaviour
         Vector3 leftRotationForce = Vector3.back * _rotationThrustPower;
         if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
         {
+            _thrusterIsActive = true;
             ApplyRotation(leftRotationForce);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
+            _thrusterIsActive = true;
             ApplyRotation(-leftRotationForce);
         }
     }
